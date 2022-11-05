8ea44e59baa86e3ce2df3257f907abb55c66d019
William Golden <wtgolden5@gmail.com>
2022-11-05 13:51:55 -0400

    how it works and who we are

---
+++ pages/how-it-works.tsx
@@ -0,0 +1,12 @@
+import { Flex, Heading, Text } from '@chakra-ui/react';
+
+const HowItWorks = () => {
+  return (
+    <Flex h='100%' alignItems='center' justifyContent='center' direction='column'>
+      <Heading>How it works</Heading>
+      <Text>It works like this</Text>
+    </Flex>
+  );
+};
+
+export default HowItWorks;
---
+++ pages/who-we-are.tsx
@@ -0,0 +1,11 @@
+import { Flex, Heading } from '@chakra-ui/react';
+
+const WhoWeAre = () => {
+  return (
+    <Flex h='100%' alignItems='center' justifyContent='center' direction='column'>
+      <Heading>Who We Are</Heading>
+    </Flex>
+  );
+};
+
+export default WhoWeAre;
