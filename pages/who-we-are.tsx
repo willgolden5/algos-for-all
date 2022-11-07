79767f208d022f1b343bae019052f6263ae7e593
William Golden <wtgolden5@gmail.com>
2022-11-06 19:25:09 -0500

    default content

--- pages/who-we-are.tsx
+++ pages/who-we-are.tsx
@@ -1,9 +1,15 @@
-import { Flex, Heading } from '@chakra-ui/react';
+import { Button, Flex, Heading, Text } from '@chakra-ui/react';
 
 const WhoWeAre = () => {
   return (
     <Flex h='100%' alignItems='center' justifyContent='center' direction='column'>
-      <Heading>Who We Are</Heading>
+      <Heading>Coming Soon...</Heading>
+      <Text mb={6}>Sign up to be notified when we go live!</Text>
+      <Flex direction='column' w='20%'>
+        <Button as='a' colorScheme='teal' type='submit' href='/signup'>
+          Get Notified
+        </Button>
+      </Flex>
     </Flex>
   );
 };
