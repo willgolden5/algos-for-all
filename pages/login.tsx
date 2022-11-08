04ed1b276803e571aeba2bf7f3c06c381926d381
William Golden <wtgolden5@gmail.com>
2022-11-08 09:18:52 -0500

    toast and state for login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,17 +1,46 @@
-import { Button, Flex, FormControl, FormLabel, Heading, Input } from '@chakra-ui/react';
+import { Button, Flex, FormControl, FormLabel, Heading, Input, useToast } from '@chakra-ui/react';
+import { useState } from 'react';
 
 const Login = () => {
+  const toast = useToast();
+  const [formState, setFormState] = useState({
+    email: '',
+    password: '',
+  });
+
+  const onSubmit = () => {
+    toast({
+      title: 'Error',
+      description: 'This feature is not yet implemented',
+      status: 'error',
+      duration: 3000,
+      isClosable: true,
+    });
+  };
+
   return (
     <Flex h='100%' alignItems='center' justifyContent='center'>
       <Flex direction='column' background='gray.200' p={10} rounded={6}>
         <Heading mb={6}>Log in</Heading>
         <FormControl>
           <FormLabel>Email</FormLabel>
-          <Input placeholder='me@email.com' variant='filled' mb={3} type='email' />
+          <Input
+            placeholder='me@email.com'
+            variant='filled'
+            mb={3}
+            type='email'
+            onChange={(e) => setFormState({ ...formState, email: e.target.value })}
+          />
           <FormLabel>Password</FormLabel>
-          <Input placeholder='******' variant='filled' mb={6} type='password' />
+          <Input
+            placeholder='******'
+            variant='filled'
+            mb={6}
+            type='password'
+            onChange={(e) => setFormState({ ...formState, password: e.target.value })}
+          />
         </FormControl>
-        <Button colorScheme='teal' type='submit' w='100%'>
+        <Button colorScheme='teal' type='submit' w='100%' onClick={() => onSubmit()}>
           Log in
         </Button>
       </Flex>
