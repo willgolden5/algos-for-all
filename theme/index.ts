3faa269adf91b2214d8b919b77b060d914e0294b
William Golden <wtgolden5@gmail.com>
2022-11-13 01:28:39 -0500

    logo

---
+++ theme/index.ts
@@ -0,0 +1,21 @@
+// theme.ts
+
+// 1. import `extendTheme` function
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+
+// 2. Add your color mode config
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+const colors = {
+  colors: {
+    black: '#16161D',
+  }
+}
+
+// 3. extend the theme
+const theme = extendTheme({ config })
+
+export default theme;
