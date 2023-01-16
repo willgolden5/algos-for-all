dd6c1427c3457cd9c50e41975cb589ee228ceb4c
William Golden <wtgolden5@gmail.com>
2023-01-15 20:48:08 -0500

    nav profile menu options

--- components/navbar/PopoverMenu.tsx
+++ components/navbar/PopoverMenu.tsx
@@ -1,23 +1,60 @@
-import { Avatar, Flex, Heading } from "@chakra-ui/react"
+import { Avatar, Box, Divider, Flex, Heading, Text } from "@chakra-ui/react"
 import { User } from "../../hooks/useUser"
 
 type PopoverMenuProps = {
     user: User
 }
 
-const PopoverMenu = ({ user }: PopoverMenuProps) => {
-    return <Flex>
-        <Flex direction='row'>
-            <Heading>{`${user.first_name} ${user.last_name}`}</Heading>
-            <Avatar
-                name={`${user.first_name} ${user.last_name}`}
-                size="sm"
-                bg="gray.700"
-                color="white"
-            />
-        </Flex>
-        <Heading></Heading>
+const MenuRow = ({ children, link }: { children: React.ReactNode, link: string }) => (
+    <Flex
+        as="a"
+        w='full'
+        direction="column"
+        px={2}
+        _hover={{ bg: "gray.100", cursor: "pointer" }}
+        href={link}
+    >
+        <Box pt={4}>{children}</Box>
+        <Divider w='100%' py={2}/>
     </Flex>
+)
+
+const PopoverMenu = ({ user }: PopoverMenuProps) => {
+    return (
+        <Flex direction="column" justify='center' align='center'>
+            <Flex direction='row' justify='center' align='center' w='full'>
+                <Avatar
+                    name={`${user.first_name} ${user.last_name}`}
+                    size="lg"
+                    bg="gray.700"
+                    color="white"
+                    m={2}
+                />
+                <Text fontSize='xl'>{`${user.first_name} ${user.last_name}`}</Text>
+            </Flex>
+            <Divider w='100%'  />
+            <MenuRow link="#">
+                <Heading as='a' w='100%' fontSize='2xl'>Account</Heading>
+            </MenuRow>
+            <MenuRow link="#">
+                <Heading as='a' w='100%' fontSize='2xl'>Settings</Heading>
+            </MenuRow>
+            <MenuRow link="#">
+                <Heading as='a' w='100%' fontSize='2xl'>Dashboard</Heading>
+            </MenuRow>
+            <MenuRow link="#">
+                <Heading as='a' w='100%' fontSize='2xl'>Trades</Heading>
+            </MenuRow>
+            <MenuRow link="#">
+                <Heading as='a' w='100%' fontSize='2xl'>Algorithms</Heading>
+            </MenuRow>
+            <Flex direction='row' justify="space-evenly" align='center' w="100%" px={4} py={4}>
+                <Text>Logout</Text>
+                <Text>Help</Text>
+                <Text>Contact</Text>
+            </Flex>
+        </Flex>
+    )
 }
 
 export default PopoverMenu
